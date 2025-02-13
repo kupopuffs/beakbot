66e348b59fb71da3ae5572c9a2ee48dbfe190cd8
Jon <pjonathanh@gmail.com>
2025-02-12 18:37:14 -0800

    fix after updates

--- README.md
+++ README.md
@@ -4,6 +4,8 @@ I wouldn't know, I stole this from [Gabriel Tanner](https://gabrieltanner.org/bl
 
 install [nodejs](https://nodejs.org/en/download/)
 
+I would use nvm (https://github.com/nvm-sh/nvm)
+
 setup a token.json like so:
 ```
 {
@@ -19,4 +21,9 @@ npm install discord.js ffmpeg fluent-ffmpeg @discordjs/opus ytdl-core --save
 run with
 ```
 node index.js
+```
+
+how to use
+```
+bb p https://www.youtube.com/watch?v=HtspUTgfgDY
 ```
--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const Discord = require("discord.js");
+const { Client, GatewayIntentBits} = require('discord.js');
 const {
     prefix,
     playCommand,
@@ -6,17 +6,31 @@ const {
     skipCommand,
     volume,
     ping
-
 } = require("./config.json");
 const { token } = require("./token.json");
-const ytdl = require("ytdl-core");
+const ytdl = require('@distube/ytdl-core');
+const { 
+    joinVoiceChannel, 
+    createAudioResource, 
+    createAudioPlayer,
+    AudioPlayerStatus
+ } = require('@discordjs/voice');
 
-const client = new Discord.Client();
 
+const client = new Client({
+    intents: [
+        GatewayIntentBits.Guilds, // Required for basic bot functionality
+        GatewayIntentBits.GuildMessages, // Allows the bot to read messages
+        GatewayIntentBits.MessageContent, // Needed to access message content
+        GatewayIntentBits.GuildVoiceStates, // Required for joining voice channels
+        3153920
+    ]
+});
 const queue = new Map();
 
 client.once("ready", () => {
     console.log("ready to gay!");
+    console.log(`Logged in as ${client.user.tag}!`);
 });
 
 client.once("reconnecting", () => {
@@ -28,7 +42,7 @@ client.once("disconnect", () => {
 });
 
 
-client.on("message", async message => {
+client.on("messageCreate", async message => {
     if (message.author.bot) return;
     if (!message.content.startsWith(prefix)) return;
 
@@ -56,6 +70,7 @@ async function pingMessage(message) {
 }
 
 async function execute(message, serverQueue) {
+    console.log('execute')
     const args = message.content.split(" ");
 
     const voiceChannel = message.member.voice.channel;
@@ -63,12 +78,15 @@ async function execute(message, serverQueue) {
         return message.channel.send(
             "You need to be in a voice channel to play music!"
         );
+
+    console.log(`has voiceChannel ${voiceChannel}`)
+
     const permissions = voiceChannel.permissionsFor(message.client.user);
-    if (!permissions.has("CONNECT") || !permissions.has("SPEAK")) {
-        return message.channel.send(
-            "I need the permissions to join and speak in your voice channel!"
-        );
-    }
+    // if (!permissions.has("CONNECT") || !permissions.has("SPEAK")) {
+    //     return message.channel.send(
+    //         "I need the permissions to join and speak in your voice channel!"
+    //     );
+    // }
 
     console.log(args[2]);
     const songInfo = await ytdl.getInfo(args[2]);
@@ -81,7 +99,7 @@ async function execute(message, serverQueue) {
         const queueContruct = {
             textChannel: message.channel,
             voiceChannel: voiceChannel,
-            connection: null,
+            connection: undefined,
             songs: [],
             volume: volume,
             playing: true
@@ -91,8 +109,16 @@ async function execute(message, serverQueue) {
 
         queueContruct.songs.push(song);
 
+        console.log('attempting connection')
+
         try {
-            var connection = await voiceChannel.join();
+            const connection = joinVoiceChannel({
+                channelId: voiceChannel.id,
+                guildId: message.guild.id,
+                adapterCreator: message.guild.voiceAdapterCreator,
+                selfDeaf: false // Set to true if you want the bot to mute itself
+            });
+    
             queueContruct.connection = connection;
             play(message.guild, queueContruct.songs[0]);
         } catch (err) {
@@ -129,22 +155,54 @@ function stop(message, serverQueue) {
 }
 
 function play(guild, song) {
+    console.log(`[play] ${JSON.stringify(song)}`)
     const serverQueue = queue.get(guild.id);
-    if (!song) {
-        // serverQueue.voiceChannel.leave();
-        queue.delete(guild.id);
+    const connection = serverQueue.connection;
+    if (!serverQueue || !song || !serverQueue.connection) {
+        console.error('invalid play args');
         return;
     }
+    
+    const player = createAudioPlayer();
+    const stream = ytdl(song.url,
+        { filter: 'audioonly' 
 
-    const dispatcher = serverQueue.connection
-        .play(ytdl(song.url))
-        .on("finish", () => {
-            serverQueue.songs.shift();
-            play(guild, serverQueue.songs[0]);
-        })
-        .on("error", error => console.error(error));
-    dispatcher.setVolumeLogarithmic(serverQueue.volume / 3);   // 1 is too loud 5 is too quiet
-    serverQueue.textChannel.send(`Start playing: **${song.title}**`);   
+        });
+    const resource = createAudioResource(stream);
+    
+    // Subscribe the connection to the player
+    connection.subscribe(player);
+
+    // Play the audio
+    player.play(resource);
+
+    // Handle playback events
+    player.on(AudioPlayerStatus.Playing, () => {
+        console.log('The audio is now playing!');
+    });
+
+    player.on(AudioPlayerStatus.Idle, () => {
+        console.log('Audio finished playing, disconnecting...');
+        connection.destroy(); // Leave the channel after playback
+    });
+
+    // message.reply('Playing audio now!');
+
+    // if (!song) {
+    //     // serverQueue.voiceChannel.leave();
+    //     queue.delete(guild.id);
+    //     return;
+    // }
+
+    // const dispatcher = serverQueue.connection
+    //     .play(ytdl(song.url))
+    //     .on("finish", () => {
+    //         serverQueue.songs.shift();
+    //         play(guild, serverQueue.songs[0]);
+    //     })
+    //     .on("error", error => console.error(error));
+    // dispatcher.setVolumeLogarithmic(serverQueue.volume / 3);   // 1 is too loud 5 is too quiet
+    // serverQueue.textChannel.send(`Start playing: **${song.title}**`);   
 }
 
 client.login(token);
